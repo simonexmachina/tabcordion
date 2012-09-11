5cffee7a77f4aaf7aa51a1413bca1568bda66baf
Simon Wade <simon.wade@gmail.com>
2012-09-11 21:37:34 +1000

    Minor changes

--- src/package.js
+++ src/package.js
@@ -21,6 +21,7 @@ if (mode.match(/postinstall|prepublish|postinstall|build|watch/)) {
   fsExists(sysPath.join(__dirname, 'tabcordion.js'), function(exists) {
     if (mode === 'postinstall' && exists) return;
     execute(['node_modules', 'coffee-script', 'bin', 'coffee'], (mode === 'watch' ? '-w ' : '') + '-o . src/');
+    execute(['node_modules', 'jade', 'bin', 'jade'], (mode === 'watch' ? '-w ' : '') + '-O . *.jade');
   });
 } else if (mode === 'test') {
   execute(['node_modules', 'mocha', 'bin', 'mocha'],
